5fb4c3b9c69a458b93cb6436dfa0291440d49b47
MRTHAKER <mahek.haker@gmail.com>
2020-07-13 13:43:59 +0530

    Genesis

---
+++ employeepro/src/spell/Checker.java
@@ -0,0 +1,11 @@
+package spell;
+public class Checker{
+	Checker(){
+		System.out.println("Inside checker const");
+		
+		
+	}
+	public void SpellCheck() {
+		System.out.println("spell checking");
+	}
+}
