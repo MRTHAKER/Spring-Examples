7d239ca52ef55343193f3b5bedff6c8984d91766
MRTHAKER <mahek.haker@gmail.com>
2020-10-27 13:17:13 +0530

    Spring-examples: Add more stuff for different operations

--- management/src/main/java/library/management/Controllers.java
+++ management/src/main/java/library/management/Controllers.java
@@ -1,5 +1,6 @@
 package library.management;
 import javax.servlet.http.*;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;  
 import org.springframework.ui.*;  
@@ -8,14 +9,25 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Controllers {
+ArrayList<String> ar;
 @RequestMapping("/login")
 public ModelAndView login(HttpServletRequest req) {
 	if(req.getParameter("name").equals("library")&& req.getParameter("pass").equals("1234")) {
-		return new ModelAndView("Add_book");
+		ar=new ArrayList<String>();
+		return new ModelAndView("book");
 	}
 	else {
 		return new ModelAndView("Wrong");
 	}
 	
+ }
+@RequestMapping("/AddBook")
+public ModelAndView AddBook(HttpServletRequest req) {
+	
+	return new ModelAndView("book");
+}
+@RequestMapping("/DeleteBook")
+public ModelAndView DeleteBook(HttpServletRequest req) {
+	return new ModelAndView("AddBook");
 }
 }
